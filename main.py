0e7804cc6c7b913f2fa9186a6724c868131be385
Akansha Negi <akanshanegi@gmail.com>
2024-11-19 21:16:39 -0600

    main python file

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Course: Introduction to Bioinformatics
+Assignment: Midterm Milestone #2
+Date: Nov 22, 2024
+
+This python3 program is designed to recursively globally align a set
+of protein sequences into one alignment.
+'''
+
